61981500ea7a42606331713e4235081b6977a18c
MatthewAlexander2002 <matthewalexander009@gmail.com>
2023-12-27 17:09:29 +1100

    added empty line between lines of text

--- src/main.py
+++ src/main.py
@@ -27,7 +27,7 @@ with open(filename, "r") as csv_file:
                 else:
                     out.write(row[column]);
                     out.write(" ")
-            out.write("\n")
+            out.write("\n\n")
         line += 1;
         
 out.close;
